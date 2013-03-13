87e452eb9169ff20bd27bef39a2e3a7eba05af15
Nathaniel Metrock <nathaniel.metrock@gmail.com>
2013-03-12 17:48:26 -0700

    Restructured classes/methods. Now generates grass.

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ Author:		Nathaniel R. Metrock
 License:	GNU GPL <http://www.gnu.org/licenses/gpl.html>
 """
 
-import sys, pygame
+import pygame
+import os
+import sys
+import random
 
 
 WIDTH = 640
 HEIGHT = 400
 GRID_X = 120
 GRID_Y = 0
-SQUARE = [['40px', '40px']]
+TILE_X = 40
+TILE_Y = 40
 BG_COLOR = [0, 0, 0]
 MAX_FPS = 30
 
+#TILE    = [slow, defense, siege, food, attack, air]
 GRASS    = [.6, .5, .75, 1, .4, .5]
 DIRT     = [.5, .6, .5, .75, 1, .75]
 WATER    = [float('inf'), 0, 0, 0, 0, 0]
 MOUNTAIN = [1, .75, 1, .5, .75, .4]
 FORT     = [.75, 1, .4, .6, .5, 1]
 
+#UNIT       = [attack, defense, group, cost, endurance, morale, health, move, level]
 ARCHERS     = [.4, .3, .7, .2, .7, .6, .5, .8, .4]
 SPEARMEN    = [.6, .8, .8, .6, .5, .5, .7, .6, .6]
 SELLSWORDS  = [.3, .7, .4, .4, .8, .2, .6, .5, .2]
@@ -38,24 +44,53 @@ TILES = [GRASS, DIRT, WATER, MOUNTAIN, FORT]
 UNITS = [ARCHERS, SPEARMEN, SELLSWORDS, KNIGHTS, CALVERY, APPRENTICES, PRIESTS, ACOLYTES, ENVOYS, KINGS]
 
 
-def Tile (tile_name, slow, defense, siege, food, attack, air):
-	10*(X + 40)
-	10*(Y + 40)
+class Generate_Map:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.name = None
 
-def Unit (unit_name, attack, defense, group, cost, endurance, morale, health, move, level):
-	unit_name = unit_name.self
+    def generate_tiles(self):
+        for i in range(len(TILES)):
+            self.tile(TILES[i])
 
-def Generate_Map(self):
-	for i in range(0, 4):
-		Tile(TILES[i])
+    def tile(self, name):
+        self.name = name
+
+        if name == GRASS:
+            for i in range(0,10):
+                for i in range(0,10):
+                    screen.blit(self.generate_grass(), (self.x, self.y))
+                    self.x += 40
+                self.x = GRID_X
+                self.y += 40
+
+    def generate_grass(self):
+        grass_tile = pygame.image.load(os.path.join("data", "grass" + str(random.randint(0,3)) + ".png")).convert()
+        return grass_tile
+
+    def unit(self, unit_name):
+    	unit_name = unit_name.self
 
 
+#start pygame & create display
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Relue Project")
+
+#create and fill background
 background = pygame.Surface(screen.get_size())
 background.fill((BG_COLOR))
 background = background.convert()
+
+#generate map & draw to screen
+grass_tile = pygame.image.load(os.path.join("data", "grass0.png")).convert()
+game_map = Generate_Map(GRID_X, GRID_Y)
 screen.blit(background, (0, 0))
+game_map.generate_tiles()
+#screen.blit(grass_tile, (GRID_X, GRID_Y))
+
+#loop variables
 clock = pygame.time.Clock()
 gameloop = True
 
@@ -71,5 +106,5 @@ while gameloop:
             if event.key == pygame.K_ESCAPE:
                 gameloop = False    # user pressed ESC
 
-    pygame.display.set_caption("Relue Project")
+    #update screen
     pygame.display.flip()
