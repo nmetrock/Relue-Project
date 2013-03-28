64ff58342e17bb106a891a2aa846bdbe52eabcd2
Nathaniel Metrock <nathaniel.metrock@gmail.com>
2013-03-27 20:26:54 -0700

    Water grid generates
    Known bugs: stops drawing water when the next location goes off the map.
    Removing the nested check fixes this but throws an out of range error
    for line 91-92. Possible fix will be adding a check for these lines that
    ignore out of range tiles.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+import random
 import pygame
 
 
@@ -17,7 +18,7 @@ pygame.display.set_caption("Relue Project")
 
 
 def generate_map():
-	grid = [['G' for x in range(10)] for y in range(10)]
+	grid = [['G' for row in range(10)] for col in range(10)]
 	x = GRID_X
 	y = 0
 	grass = pygame.image.load(os.path.join("data","tiles",
@@ -31,25 +32,99 @@ def generate_map():
 	fort = pygame.image.load(os.path.join("data","tiles",
 											"fort.png")).convert()
 
+	water_grid = []
+	w_row = random.randint(0,9)
+	if w_row==0 or w_row==9:
+		w_col = random.randint(0,9)
+	else:
+		w_col = random.randrange(0,10,9)
+	water_grid.append((w_row, w_col))
+	coming_from = None
+	draw_water = True
+	while draw_water:
+		if len(water_grid)<16:
+			next_location = True
+		while next_location:
+			r_direct = random.randrange(4)
+			r_dist = random.randint(2,4)
+			if r_direct == coming_from:
+				next_location = False
+			if (not off_map(w_row,w_col,r_direct,r_dist) and
+				len(water_grid)<=8) or (len(water_grid)>8):
+				if r_direct == 0:
+					for i in range(r_dist):
+						if not off_map(w_row,w_col,r_direct,r_dist):
+							w_row -= 1
+							water_grid.append((w_row, w_col))
+							coming_from = 2
+							next_location = False
+						else:
+							draw_water = False
+				elif r_direct == 1:
+					for i in range(r_dist):
+						if not off_map(w_row,w_col,r_direct,r_dist):
+							w_col += 1
+							water_grid.append((w_row, w_col))
+							coming_from = 3
+							next_location = False
+						else:
+							draw_water = False
+				elif r_direct == 2:
+					for i in range(r_dist):
+						if not off_map(w_row,w_col,r_direct,r_dist):
+							w_row += 1
+							water_grid.append((w_row, w_col))
+							coming_from = 0
+							next_location = False
+						else:
+							draw_water = False
+				else:
+					for i in range(r_dist):
+						if not off_map(w_row,w_col,r_direct,r_dist):
+							w_col -= 1
+							water_grid.append((w_row, w_col))
+							coming_from = 1
+							next_location = False
+						else:
+							draw_water = False
+	print water_grid
+	for row, col in water_grid:
+		grid[row][col] = 'W'
+
 	for row in range(10):
-		for column in range(10):
-			if grid[row][column] == 'G':
+		for col in range(10):
+			if grid[row][col] == 'G':
 				screen.blit(grass, (x, y))
-			elif grid[row][column] == 'W':
+			elif grid[row][col] == 'W':
 				screen.blit(water, (x, y))
-			elif grid[row][column] == 'D':
+			elif grid[row][col] == 'D':
 				screen.blit(dirt, (x, y))
-			elif grid[row][column] == 'M':
+			elif grid[row][col] == 'M':
 				screen.blit(mountain, (x, y))
-			elif grid[row][column] == 'F':
+			elif grid[row][col] == 'F':
 				screen.blit(fort, (x, y))
 			x += 40
 		x = GRID_X
 		y += 40
  
+def off_map(row, col, direction, distance):
+ 	if direction == 0:
+ 		if row - distance < 0:
+ 			return True
+ 	elif direction == 1:
+ 		if col + distance > 9:
+ 			return True
+ 	elif direction == 2:
+ 		if row + distance > 9:
+ 			return True
+ 	else:
+ 		if col - distance < 0:
+ 			return True
+
 
 done = False
 clock = pygame.time.Clock()
+first_run = True
 
 while done == False:
 	# EVENT PROCESSING
@@ -66,8 +141,10 @@ while done == False:
 
  
     # DRAW CODE
-	screen.fill(black)
-	generate_map()
+	if first_run:
+		screen.fill(black)
+		generate_map()
+		first_run = False
 
     pygame.display.flip()
  
